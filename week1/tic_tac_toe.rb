579efee6c5e01ad7a457d8e8a95b62eaaa537300
Frenzy277 <tomas.tomecek@hotmail.com>
2014-10-16 18:06:20 +0200

    Fix tic tac toe - tie introduced, refactor winning_lines completely and initial positions hash, introduce ceremony

--- week1/tic_tac_toe.rb
+++ week1/tic_tac_toe.rb
@@ -1,8 +1,8 @@
 # Tic Tac Toe assignment!
 # Start up definitions.
-positions = { '1' => ' ', '2' => ' ', '3' => ' ', 
-              '4' => ' ', '5' => ' ', '6' => ' ', 
-              '7' => ' ', '8' => ' ', '9' => ' ' }
+WINNING_LINES = [%w(1 2 3),%w(4 5 6),%w(7 8 9),%w(1 2 3),%w(1 5 9),%w(7 5 3),%w(1 4 7),%w(2 5 8), %w(3 6 9)]
+positions = {}
+(1..9).each { |p| positions[p.to_s] = ' ' }
 available = positions.keys
 
 # Board.
@@ -22,23 +22,27 @@ def lattice(positions)
   puts ""
 end
 
+# Shows winner or tie.
+def ceremony(positions, result, obj)
+  lattice(positions)
+  result == 'win' ? puts("#{obj} won!") : puts("It's a tie!")
+  exit
+end
+
 # Checks whether 3 x's or 3 o's consecutively are taken.
-def taken?(p1, p2, p3, positions, obj)
+def taken?(line, p, obj)
   x_or_o = 'X' if obj == "Player"
   x_or_o = 'O' if obj == "Computer"
-  (positions[p1] == x_or_o) && (positions[p2] == x_or_o) && (positions[p3] == x_or_o)
+  (p[line[0]] == x_or_o) && (p[line[1]] == x_or_o) && (p[line[2]] == x_or_o)
 end
 
 # Evaluates winning combo.
-def evaluate_winner(positions, obj)
-  if taken?('1','2','3', positions, obj) || taken?('4','5','6', positions, obj) || 
-     taken?('7','8','9', positions, obj) || taken?('1','5','9', positions, obj) || 
-     taken?('7','5','3', positions, obj) || taken?('1','4','7', positions, obj) || 
-     taken?('2','5','8', positions, obj) || taken?('3','6','9', positions, obj)
-    lattice(positions)
-    puts "#{obj.capitalize} won!"
-    exit
+def evaluate_winner(available, positions, obj)
+  WINNING_LINES.each do |line|
+    ceremony(positions, 'win', obj) if taken?(line, positions, obj)
   end
+
+  ceremony(positions, 'tie', obj) if available.empty?
 end
 
 # Marks position to lattice and evaluates winner right after.
@@ -46,7 +50,7 @@ def execute_pick(available, positions, pick, obj)
   available.delete(pick)
   positions[pick] = 'X' if obj == "Player"
   positions[pick] = 'O' if obj == "Computer"
-  evaluate_winner(positions, obj)
+  evaluate_winner(available, positions, obj)
 end
 
 loop do
@@ -58,7 +62,7 @@ loop do
   end until available.include?(player_pick)
   execute_pick(available, positions, player_pick, "Player")
   
-  # Computer-easy picks a position.
+  # Computer picks a position.
   comp_pick = available.sample
   execute_pick(available, positions, comp_pick, "Computer")
 end
